f5a640dbe1437c02b3356a372a519bb48667297c
AbdurRahman Khan <weused@gmail.com>
2025-10-20 20:01:34 +0500

    setup authentication dependency for secure routes

--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -3,10 +3,12 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.models import Users
 
+from app.dependencies.auth import auth_dependency
+
 router = APIRouter()
 
 
-@router.get("/users")
+@router.get("/users", dependencies=[Depends(auth_dependency)])
 def get_users(db: Session = Depends(get_db)):
     users = db.query(Users).all()
     return users
---
+++ backend/app/dependencies/auth.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request, HTTPException, status
+
+from app.core.security import decode_token
+
+app = FastAPI()
+
+
+def auth_dependency(request: Request):
+    # Extract Authorization header
+    auth_header = request.headers.get("Authorization")
+
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Forbidden: Invalid or Missing authorization header"
+        )
+
+    # Extract token from header
+    token = auth_header.split("Bearer ")[1]
+
+    # Verify the token
+    payload = decode_token(token)
+    print(payload)
+
+    # Exception on no payload
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Forbidden: Invalid or Expired token"
+        )
+    return payload
