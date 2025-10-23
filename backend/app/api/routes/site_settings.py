75e41687bf9389170a60cb12d41d5cae853558f3
AbdurRahman Khan <weused@gmail.com>
2025-10-23 17:08:16 +0500

    Removed auth dependency from site-settings

--- backend/app/api/routes/site_settings.py
+++ backend/app/api/routes/site_settings.py
@@ -3,12 +3,10 @@ from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.models.site_settings import SiteSettings
 
-from app.dependencies.auth import auth_dependency
-
 router = APIRouter()
 
 
 @router.get("/settings")
-def get_users(db: Session = Depends(get_db), payload: dict = Depends(auth_dependency)):
+def get_users(db: Session = Depends(get_db)):
     settings = db.query(SiteSettings).all()
     return settings
