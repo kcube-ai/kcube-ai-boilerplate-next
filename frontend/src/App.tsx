f1d71a2651544dfd63aab19bd1b4368987a4bdfc
AbdurRahman Khan <weused@gmail.com>
2025-10-23 22:57:23 +0500

    Added the email verification page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import SignupPage from './pages/auth/Signup'
+import VerifyPage from './pages/auth/VerifyPage'
 
 function App() {
   return (
     <Routes>
+      <Route path="/verify" element={<VerifyPage />} />
       <Route path="/signup" element={<SignupPage />} />
     </Routes>
   )
